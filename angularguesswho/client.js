16047572645da90d90c7ddb9ce4c478012707525
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-26 16:57:17 -0500

    pictures and objects created

--- angularguesswho/client.js
+++ angularguesswho/client.js
@@ -1,1 +1,13 @@
 var myApp = angular.module('myApp',[]);
+
+myApp.controller('GuessWhoController', function(){
+
+    var guess = this;
+    
+    var people = [{name: 'Laura', github: 'goetsit', show:true}
+                ,{name: 'Chad', github: 'ChadRoesler',show:true}];
+
+    guess.message = 'Who is pictured?';
+
+    guess.people = people;
+});
