cef6f8747a569c4c20f1256fa7880b9184510c0c
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-26 21:17:04 -0500

    console.logs the name

--- angularguesswho/client.js
+++ angularguesswho/client.js
@@ -3,6 +3,11 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('GuessWhoController', function(){
 
     var guess = this;
+
+
+    guess.test = function(person) {
+        console.log(person);
+    }
     
     var people = [{name: 'Laura', github: 'goetsit', show:true}
                 ,{name: 'Chad', github: 'ChadRoesler',show:true}];
